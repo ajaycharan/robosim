9ba258172d9fb0efcc415d4dd07baf6f2cf2bfd8
gareth-cross <gcross.code@icloud.com>
2014-05-13 15:01:43 -0400

    Added test method

--- Simulator.m
+++ Simulator.m
@@ -16,9 +16,36 @@ classdef Simulator < handle
         
         function step(self)
             
+            % ODE options
+            options = odeset('RelTol', 1e-2, 'AbsTol', 1e-4);
             
+            for i=1:numel(self.robots)
+                
+                X = self.robots(i).X;
+                U = self.robots(i).U;
+                
+                % integrate
+                [T,X] = ode45(@(t,x)Simulator.dynamics(t,x,U), [0 self.t_step], X, options);
+                self.robots(i).X = X(end,:)';
+            end
             
+            % perform control
+            for i=1:numel(self.robots)
+                self.robots(i).U = self.robots(i).controller( self.robots(i) );
+            end
+                        
         end        
+    end
+    
+    methods(Static)
+       
+        function Xdot = dynamics(~,X,U)
+            Xdot = zeros(3,1);
+            Xdot(1) = U(1) * cos(X(3));
+            Xdot(2) = U(1) * sin(X(3));
+            Xdot(3) = U(2);
+        end
+        
     end
     
 end
