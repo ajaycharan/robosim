c2e8396a6829b868e664327a2f4021dc56d519ca
Chao Qu <quchao@seas.upenn.edu>
2014-05-13 14:23:01 -0400

    add interfaces for Simulator class

--- Simulator.m
+++ Simulator.m
@@ -1,9 +1,23 @@
 classdef Simulator < handle
 
     properties
+        robots
     end
     
     methods
+        
+        function Simulator()
+            
+        end
+        
+        function simulate(self)
+            
+        end
+        
+        function step(self)
+            
+        end
+        
     end
     
 end
