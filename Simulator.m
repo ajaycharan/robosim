fed07f316e8e8cfae0b9ecec43a30fd8c613a0c3
gareth-cross <gcross.code@icloud.com>
2014-05-13 15:21:43 -0400

    Added some plotting stuff

--- Simulator.m
+++ Simulator.m
@@ -2,6 +2,8 @@ classdef Simulator < handle
 
     properties
         t_step = 0.01;
+        t_max = 0;
+        t_cur = 0;
         robots = Robot.empty();
     end
     
@@ -10,22 +12,48 @@ classdef Simulator < handle
     end
     
     methods
-        function self = Simulator(t_range)
-            hFigure = figure;
+        function self = Simulator(t_max)
+            self.hFigure = figure;
             
+            if t_max <= 0
+                error('t_max must be >= 0');
+            end
+            self.t_max = t_max;
             
         end
         
         function simulate(self)
             
+            % configure plot
+            figure(self.hFigure);
+            plot(0,0);  % empty
+            hold on;
+            grid on;
+            set(gca, 'Box', 'On');
             
-            
+            % run simulation
+            while self.t_cur < self.t_max
+                [min_pos, max_pos] = self.step();
+                
+                % reshape axes
+                axis([min_pos(1)-1 max_pos(1)+1 min_pos(2)-1 max_pos(2)+1]);
+                
+                % update all robot plots
+                self.robots.plot();
+            end            
         end
         
-        function step(self)
+        function [min_pos, max_pos] = step(self)
             
             % ODE options
             options = odeset('RelTol', 1e-2, 'AbsTol', 1e-4);
+            
+            t_start = self.t_cur;
+            t_final = self.t_cur + self.t_step;
+            
+            % min/max positions
+            min_pos = [ Inf  Inf];
+            max_pos = [-Inf -Inf];
             
             for i=1:numel(self.robots)
                 
@@ -33,16 +61,20 @@ classdef Simulator < handle
                 U = self.robots(i).U;
                 
                 % integrate
-                [T,X] = ode45(@(t,x)Simulator.dynamics(t,x,U), [0 self.t_step], X, options);
+                [T,X] = ode45(@(t,x)Simulator.dynamics(t,x,U), [t_start t_final], X, options);
                 self.robots(i).updateState(T(end), X(end,:)');
+                
+                min_pos = min(min_pos, [X(1) X(2)]);
+                max_pos = max(max_pos, [X(1) X(2)]);
             end
+            self.t_cur = t_final;
             
             % perform control
             for i=1:numel(self.robots)
-                self.robots(i).controller( self.robots(i) );
+                self.robots(i).controller( t_final, self.robots(i) );
             end
                         
-        end        
+        end
     end
     
     methods(Static)
--- Test.m
+++ Test.m
@@ -3,11 +3,10 @@ clear;
 close all;
 home;
 
-sim = Simulator();
+sim = Simulator(10);
 
 sim.robots(1) = Robot();
 sim.robots(2) = Robot();
 
-for i=1:100
-    sim.step();
-end
+sim.simulate();
+
